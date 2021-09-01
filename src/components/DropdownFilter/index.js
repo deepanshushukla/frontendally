fe0d1a70df27a7ee53bfe43fead0a5750d66239d
deepanshu <deepanshus@observe.ai>
2021-09-01 18:10:51 +0530

    fix : added css

--- src/components/DropdownFilter/index.js
+++ src/components/DropdownFilter/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import PropTypes from 'prop-types';
-
+import './inde.scss'
 const DropdownFilter = ({ changeCategory,categoriesValues }) => {
     const category = useSelector((state) => state.okr.category);
 
     return (
-        <select value={category} onChange={(event) => changeCategory(event.target.value)}>
+        <select className='dropdown' value={category} onChange={(event) => changeCategory(event.target.value)}>
             <option value="" disabled >
                 Select Category
             </option>
